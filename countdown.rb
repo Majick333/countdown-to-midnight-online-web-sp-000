dbb15af5639474cdb9bb8a9f54007e52646ad32e
Ryan M <rmccauleymusic@gmail.com>
2019-10-18 01:29:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,11 +5,11 @@ def countdown(seconds)
     while seconds > 0
       puts "#{seconds} SECOND(S)!"
       seconds -= 1
-      return "HAPPY NEW YEAR!"
+      
 end
 end
 
-puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
 
 def countdown_with_sleep(countdown)
   while countdown < 0
