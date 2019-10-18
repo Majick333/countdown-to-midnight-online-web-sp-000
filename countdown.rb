f1c7c6f0c5d5204ab875ff3d0ab3f55f50d8914f
Ryan M <rmccauleymusic@gmail.com>
2019-10-18 00:46:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+  seconds = 20
+    while seconds > 0
+      puts "#{seconds} until midnight"
+      seconds -= 1
+      if seconds == 0
+        puts "HAPPY NEW YEAR!"
+end
 end
